8618d1191e3607d57f4ae2e9cb39e29cde4de679
EdwardGonzalez98 <gonzalezedward014@gmail.com>
2018-06-05 19:45:33 -0400

    Add files via upload

---
+++ Myonlinelibrary/inc/data.php
@@ -0,0 +1,165 @@
+<?php
+//Books
+$catalog[101] = [
+    "title" => "The Three Little Pigs",
+    "img" => "img/media/Threepigs.jpg",
+   "genre" => "Fable",
+   "format" => "Paperback",
+   "year" => 1890,
+   "category" => "Books",
+   "authors" => [
+       "James Halliwell-Phillipps",
+
+   ],
+   "publisher" => "Haarper and Row",
+   "isbn" => '9781577683674'
+];
+
+   "title" => "Pinocchio",
+   "img" => "img/media/pinocchio.jpg",
+   "genre" => "Fiction, Literature, Fantasy",
+   "format" => "Paperback",
+   "year" => 1883,
+   "category" => "Books",
+   "authors" => [
+   "Carlo Collodi",
+],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9781577998143'
+
+];
+   "title" => "Robin Hood",
+   "img" => "img/media/robinhood.jpg",
+   "genre" => "Drama, Action, film",
+   "format" => "Paperback",
+   "year" => 1883,
+   "category" => "Books",
+   "authors" => [
+   "Howard Pyle",
+
+],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9781577998143'
+];
+   "title" => "Christmas",
+   "img" => "img/media/christmas.jpg",
+   "genre" => "Children Literature",
+   "format" => "Paperback",
+   "year" => 1823,
+   "category" => "Books",
+   "authors" => [
+   "Clement Clarke Moore",
+
+],
+   "publisher" => "Haarper and Row",
+   "isbn" => '9781577683674'
+];
+*//Movies*
+$catalog[201] = [
+   "title" => "Titanic",
+   "img" => "img/media/Titanic.jpg",
+   "genre" => "Romance Film,Drama,Disaster Film",
+   "format" => "DVD",
+   "year" => 1997,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Leonardo DiCaprio",
+       "Kate Winslet",
+       "Billy Zane"
+   ]
+];
+   "title" => "Home Alone",
+   "img" => "img/media/Home alone.jpg",
+   "genre" => "Comedy,Family film",
+   "format" => "DVD",
+   "year" => 1990,
+   "category" => "Movies",
+   "director" => "Chris Columbus",
+   "writers" => [
+    "John Hughes"
+
+],
+"stars" => [
+    "Macaulay Culkin",
+    "Daniel Stern",
+    "Joe Pesci"
+]
+];
+
+   "title" => "Scarface",
+   "img" => "img/media/scarface.jpg",
+   "genre" => "Action,Adventure",
+   "format" => "DVD",
+   "year" => 1983,
+   "category" => "Movies",
+   "director" => "Brian De Palma",
+   "writers" => [
+    "Oliver Stone"
+
+],
+"stars" => [
+    "Al pacino",
+    "Michelle Pfeiffer",
+    "Steven Bauer"
+]
+];
+
+   "title" => "Avengers",
+   "img" => "img/media/avengers.jpg",
+   "genre" => "Action,Adventure",
+   "format" => "DVD",
+   "year" => 2018,
+   "category" => "Movies",
+   "director" => "Anthony, Joe Russo",
+   "writers" => [
+    "Joe russo, Anthony russo"
+
+],
+"stars" => [
+    "Robert Downey JR.",
+    "Chris Hemsworth",
+    "Chris Evans"
+]
+];
+
+
+*//Music*
+$catalog[301] = [
+   "title" => "If your reading this it's too late",
+   "img" => "img/media/drake.jpg",
+   "genre" => "HIP-HOP",
+   "format" => "CD",
+   "year" => 2015,
+   "category" => "Music",
+   "artist" => "Drake"
+];
+   "title" => "KOD",
+   "img" => "img/media/jcole.jpg",
+   "genre" => "HIP-HOP",
+   "format" => "CD",
+   "year" => 2018,
+   "category" => "Music",
+   "artist" => "Jcole"
+];
+   "title" => "Recovery",
+   "img" => "img/media/eminem.jpg",
+   "genre" => "HIP-HOP",
+   "format" => "CD",
+   "year" => 2010,
+   "category" => "Music",
+   "artist" => "Eminem"
+];
+   "title" => "All Eyez on Me",
+   "img" => "img/media/2pac.jpg",
+   "genre" => "HIP-HOP",
+   "format" => "CD",
+   "year" => 1996,
+   "category" => "Music",
+   "artist" => "2pac"
+];
+?>
---
+++ Myonlinelibrary/inc/footer.php
@@ -0,0 +1,8 @@
+</div><!-- end content -->
+
+  <div class="footer">
+
+    <div class="wrapper">
+
+      <ul>
+        <li><a href="https://instagram.com//LaguardiaCC/">Twitter</a></li>
